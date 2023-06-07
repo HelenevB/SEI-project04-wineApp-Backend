801f30a2e868a642366b1387399dd82ad52e9586
hvb <helene.vanbesouw@gmail.com>
2023-06-07 14:03:01 +0100

    deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const bodyParser = require('body-parser')
 const path = require('path')
 
 // Port Configuration
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5400 ;
 
 
 // Initialze Express
